c7014defe2099171f64d43d53c1b525293a36c45
Sara <saranatourcontact@gmail.com>
2023-02-28 15:16:45 +0200

    Fixed DojoWeather Assignment

--- 2-JavaScript section/Day 9/2-Dojo Weather/script.js
+++ 2-JavaScript section/Day 9/2-Dojo Weather/script.js
@@ -2,28 +2,30 @@
 var cookie = document.querySelector(".cookies");
 // Function to alert that the city has been clicked
 function alertOfname(city) {
-	alert(`You have clicked on the city of ${city.innerText} ,Loading Weather report`);
-	console.log(city.innerText);
+  alert(
+    `You have clicked on the city of ${city.innerText} ,Loading Weather report`
+  );
+  console.log(city.innerText);
 }
 
 function removeVisibilaty() {
-	cookie.style.visibility = "hidden";
+  cookie.style.visibility = "hidden";
 }
 setTimeout(() => {
-	cookie.style.visibility = "visible";
+  cookie.style.visibility = "visible";
 }, 1000);
 
-function changeTemperature(elem, tempVal) {
-	var tempVal = document.querySelectorAll(".temp");
-	for(var i = 0; i < tempVal.length; i++) {
-		if(elem.value == "F") {
-			var val = tempVal[i].innerText;
-			tempVal[i].innerText = Math.round(val * (9 / 5) + 32);
-			console.log(tempVal[i].innerText);
-		} else if(elem.value == "C") {
-			var val2 = tempVal[i].innerText;
-			tempVal[i].innerText = Math.round(((val2 - 32) * 5) / 9);
-			console.log(tempVal[i].innerText);
-		}
-	}
+function changeTemperature(elem) {
+  var tempVal = document.querySelectorAll(".temp");
+  if (elem.value == "F") {
+    tempVal.forEach((element) => {
+      var val = element.innerText;
+      element.innerText = Math.round(val * (9 / 5) + 32);
+    });
+  } else if (elem.value == "C") {
+    tempVal.forEach((element) => {
+      var val2 = element.innerText;
+      element.innerText = Math.round(((val2 - 32) * 5) / 9);
+    });
+  }
 }
